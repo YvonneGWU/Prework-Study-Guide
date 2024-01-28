722b7081474ee3b9a2146c96a63c624bc8b1d5cd
Yvonne Sidell <ceo@mobilecertservices.com>
2024-01-28 11:50:19 -0500

    Moving listTopics to bottom of script

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,8 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var x = 0; x < topics.length; x++) {
-  console.log(topics[x]);
- }
- 
- /*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
+
+function selectTopic() {
+  if (topic === 'HTML') {
+    console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
  console.log("Let's study CSS!");
 } else if (topic === 'Git') {
@@ -15,4 +12,9 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
+}
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+}
